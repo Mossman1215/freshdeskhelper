2a55af666e4dcc0a18c259dd20d798724709081e
Moss Cantwell <moss@silverstripe.com>
2019-09-16 16:32:32 +1200

    logging

--- src/Helper.php
+++ src/Helper.php
@@ -7,9 +7,9 @@ use GuzzleHttp\Psr7\Request;
 use freshdeskhelper\CredentialProvider;
 
 class Helper{
-   
+    
     public function getTickets(){
-        
+        $logger= $GLOBALS['logger']->info('getting tickets');
         $client = new Client([
             'base_uri' => 'https://silverstripe.freshdesk.com/api/v2/',
             'timeout'  => 2.0,
@@ -22,19 +22,29 @@ class Helper{
         
         $response = $client->send($request);
         if ($response->getStatusCode() == 200) {
+            $GLOBALS['logger']->info('request success');
             return json_decode($response->getBody()->getContents(),true);
+        }else{
+            $GLOBALS['logger']->error('failure to request tickets '. $response->getStatusCode().PHP_EOL.$response->getReasonPhrase());
         }
     }
     public function updateAllNSADates(){
         $today = new DateTime('now');
         $listoftickets = Helper::getTickets();
+        $ticketsToUpdate =[];
         foreach ($listoftickets['results'] as $ticket) {
             $ticketNSA = DateTime::createFromFormat('Y-m-d',$ticket['custom_fields']['cf_next_scheduled_action']);
             if ($ticketNSA < $today) {
                 #add id to tickets to update list
-                echo 'ticket needs to be updated ' . $ticket['id'] . PHP_EOL;
+                array_push($ticketsToUpdate,$ticket['id']);
             }
+        
         }
+        $GLOBALS['logger']->info(count($ticketsToUpdate).' tickets to update');
+        echo var_export($listoftickets).PHP_EOL;
+        echo var_export($ticketsToUpdate).PHP_EOL;
+        echo 'date to update to'.PHP_EOL;
+        echo json_encode(['custom_fields'=>['cf_next_scheduled_action'=> date('Y-m-d',$today->getTimestamp())]]).PHP_EOL;
     } 
     public function displayTickets(){
         $listoftickets = Helper::getTickets();
