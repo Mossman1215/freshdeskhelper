38b1b35f85ee12e82a3003d18d08b42204b6ce27
Moss Cantwell <moss@silverstripe.com>
2019-09-19 14:15:03 +1200

    browserkit experiment

---
+++ bin/mantisscrape.php
@@ -0,0 +1,36 @@
+#!/usr/bin/env php
+<?php
+$autoloaded = false;
+$paths = [
+    __DIR__.'/../vendor/autoload.php',
+    __DIR__.'/../../../vendor/autoload.php',
+];
+foreach ($paths as $path) {
+    if (file_exists($path)) {
+        require_once $path;
+        $autoloaded = true;
+
+        break;
+    }
+}
+if (false === $autoloaded) {
+    throw new Exception('Please run composer install to set up project dependencies');
+}
+use Symfony\Component\BrowserKit\HttpBrowser;
+use Symfony\Component\HttpClient\HttpClient;
+$browser = new HttpBrowser(HttpClient::create());
+$browser->request('GET', 'https://cwp.govt.nz/service-desk');
+print(trim($browser->getResponse()->getContent()).PHP_EOL);
+
+// select the form and fill in some values
+$form = $crawler->selectButton('Log in')->form();
+$form['login'] = 'symfonyfan';
+$form['password'] = 'anypass';
+
+// submit that form
+$crawler = $client->submit($form);
+
+// $browser->submitForm('Sign in', ['login' => '...', 'password' => '...']);
+// $openPullRequests = trim($browser->clickLink('Pull requests')->filter(
+//     '.table-list-header-toggle a:nth-child(1)'
+// )->text());
