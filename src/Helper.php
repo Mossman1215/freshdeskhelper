3c1a2b69e90882f8eebda278178c795159b6edc7
Moss Cantwell <moss@silverstripe.com>
2019-09-26 14:21:26 +1200

    implement commands and fix autoloader

--- src/Helper.php
+++ src/Helper.php
@@ -5,37 +5,39 @@ use DateTime;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Promise;
-use freshdeskhelper\CredentialProvider;
 
-class Helper{
+class Helper
+{
     /**
-     * 
+     *
      * @return array
      */
-    public function freshdeskrequest(string $authString,Client $client,int $page){
+    public function freshdeskrequest(string $authString, Client $client, int $page)
+    {
         $windowConstant = 7890000;
         $window = time()- $windowConstant;
-        $date = substr(date(DateTime::ATOM,$window),0,19).'Z';
-        $request = new Request('GET','tickets?updated_since='.$date.'&per_page=100&page='.$page,[
+        $date = substr(date(DateTime::ATOM, $window), 0, 19).'Z';
+        $request = new Request('GET', 'tickets?updated_since='.$date.'&per_page=100&page='.$page, [
             'Authorization' => ['Basic '. $authString],
         ]);
         
         $response = $client->send($request);
         if ($response->getStatusCode() == 200) {
             $GLOBALS['logger']->info('request success getting page '.$page);
-            $newAndMyTickets = json_decode($response->getBody()->getContents(),true);
-            if(count($newAndMyTickets)>0){
-                return array_merge($newAndMyTickets,Helper::freshdeskrequest($authString,$client,$page+1));
-            }else{
+            $newAndMyTickets = json_decode($response->getBody()->getContents(), true);
+            if (count($newAndMyTickets)>0) {
+                return array_merge($newAndMyTickets, Helper::freshdeskrequest($authString, $client, $page+1));
+            } else {
                 return $newAndMyTickets;
             }
-        }else{
+        } else {
             $GLOBALS['logger']->error('failure to request page of tickets '. $response->getStatusCode().PHP_EOL.$response->getReasonPhrase());
             return [];
         }
     }
     
-    public function getTickets(){
+    public function getTickets()
+    {
         $GLOBALS['logger']->info('getting tickets');
         $client = new Client([
             'base_uri' => 'https://silverstripe.freshdesk.com/api/v2/',
@@ -43,22 +45,23 @@ class Helper{
         ]);
         $creds = CredentialProvider::fromini();
         $authString = base64_encode($creds[0].':X');
-        $newAndMyTickets = Helper::freshdeskrequest($authString,$client,$page = 1);
-        if(count($newAndMyTickets)==0){
+        $newAndMyTickets = Helper::freshdeskrequest($authString, $client, $page = 1);
+        if (count($newAndMyTickets)==0) {
             print('failure to retrieve tickets'.PHP_EOL);
         }
-        $myTickets = array_filter($newAndMyTickets,function ($ticket) use($creds){
-            if($ticket['status'] == 5){
+        $myTickets = array_filter($newAndMyTickets, function ($ticket) use ($creds) {
+            if ($ticket['status'] == 5) {
                 return false;
             }
-            if($ticket['status'] == 4){
+            if ($ticket['status'] == 4) {
                 return false;
             }
             return $ticket["responder_id"] == $creds[1];
         });
         return $myTickets;
     }
-    public function updateAllNSADates(){
+    public function updateAllNSADates()
+    {
         $client = new Client([
             'base_uri' => 'https://silverstripe.freshdesk.com/api/v2/',
             'timeout'  => 5.0,
@@ -67,32 +70,33 @@ class Helper{
         $today = new DateTime('now');
         $listoftickets = Helper::getTickets();
         $ticketsToUpdate =[];
-        $todayNSADate = ['custom_fields'=>['cf_next_scheduled_action'=> date('Y-m-d',$today->getTimestamp())]];
+        $todayNSADate = ['custom_fields'=>['cf_next_scheduled_action'=> date('Y-m-d', $today->getTimestamp())]];
         foreach ($listoftickets as $ticket) {
-            $ticketNSA = DateTime::createFromFormat('Y-m-d',$ticket['custom_fields']['cf_next_scheduled_action']);
+            $ticketNSA = DateTime::createFromFormat('Y-m-d', $ticket['custom_fields']['cf_next_scheduled_action']);
             if ($ticketNSA < $today) {
                 #add id to tickets to update list
-                $ticketsToUpdate[$ticket['id']] = $client->requestAsync('PUT','tickets/'.$ticket['id'],[
+                $ticketsToUpdate[$ticket['id']] = $client->requestAsync('PUT', 'tickets/'.$ticket['id'], [
                     'auth' => [$creds[0],'x', 'basic'],
                     'json' => $todayNSADate
                 ]);
             }
         }
         $GLOBALS['logger']->info(count($ticketsToUpdate).' tickets to update NSA');
-        $GLOBALS['logger']->info('updating NSA on ticket(s):'.implode(",",array_keys($ticketsToUpdate)));
+        $GLOBALS['logger']->info('updating NSA on ticket(s):'.implode(",", array_keys($ticketsToUpdate)));
         
         $results = Promise\unwrap($ticketsToUpdate);
         foreach (array_keys($ticketsToUpdate) as $ticketID) {
             $response = $results[$ticketID];
             #how to get status code from results array?
-            if($response->getStatusCode() == 200){
+            if ($response->getStatusCode() == 200) {
                 $GLOBALS['logger']->info('updated ticket:'.$ticketID);
-            }else{
+            } else {
                 $GLOBALS['logger']->error('failure to update ticket:'.$ticketID.$response->getReasonPhrase());
             }
         }
-    } 
-    public function displayTickets(array $listoftickets){
+    }
+    public function displayTickets(array $listoftickets)
+    {
         #print ticket summary as symfony console table
         foreach ($listoftickets['results'] as $ticket) {
             echo $ticket['subject'] . PHP_EOL;
